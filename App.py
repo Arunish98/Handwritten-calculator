dcad3528aacdbeb388d61f93d6e359120dc2fa0b
Arunish Singh <singh.arunish1@gmail.com>
2020-01-27 23:19:01 +0530

    New style

--- App.py
+++ App.py
@@ -1,7 +1,8 @@
 from kivy.app import App
+from kivy.uix.gridlayout import GridLayout
+from kivy.graphics import Color, Ellipse, Line
 from kivy.uix.widget import Widget
 from kivy.uix.button import Button
-from kivy.graphics import Color, Ellipse, Line
 
 
 class MyPaintWidget(Widget):
@@ -16,21 +17,34 @@ class MyPaintWidget(Widget):
         touch.ud['line'].points += [touch.x, touch.y]
 
 
-class MyPaintApp(App):
 
-    def build(self):
-        parent = Widget()
+class MyGrid(GridLayout):
+    def __init__ (self, **kwargs):
+        super(MyGrid,self).__init__(**kwargs)
+        self.cols = 1
+        
+        self.inside = GridLayout()
         self.painter = MyPaintWidget()
-        clearbtn = Button(text='Clear',  font_size=12, size=(70, 30), pos=(350, 20))
-        #gumb = Button(text='SEND', font_size=12, size_hint=(None, None), size=(200, 50), pos=(200, 200))
-        clearbtn.bind(on_release=self.clear_canvas)
-        parent.add_widget(self.painter)
-        parent.add_widget(clearbtn)
-        return parent
+        self.inside.add_widget(self.painter)
+        self.add_widget(self.inside)
+
+        self.clearbtn = Button(text='Clear',  font_size=42)
+        self.clearbtn.bind(on_release=self.clear_canvas)
+        self.add_widget(self.clearbtn)
+
 
     def clear_canvas(self, obj):
         self.painter.canvas.clear()
+        
 
 
-if __name__ == '__main__':
-    MyPaintApp().run()
+
+
+
+class MyApp(App):
+    def build(self):
+        return MyGrid()
+
+
+if __name__ ==  "__main__":
+    MyApp().run()
