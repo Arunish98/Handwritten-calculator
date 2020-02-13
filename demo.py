e54a66b6b6aa056fe691e47a574659909a74b55a
Arunish Singh <singh.arunish1@gmail.com>
2020-02-13 09:46:13 +0530

    still on it

--- demo.py
+++ demo.py
@@ -7,21 +7,26 @@ from kivy.uix.boxlayout import BoxLayout
 
 
 
-class LayItOn(BoxLayout):
+
+class MyPaintWidget(Widget):
+
     def on_touch_down(self, touch):
         with self.canvas.before:
-            Color(1,1,1,1)
+            Color(1,0,0,1)
             
             touch.ud['line'] = Line(points=(touch.x, touch.y),width=2)
-            
             
 
     def on_touch_move(self, touch):
         touch.ud['line'].points += [touch.x, touch.y]
-        
+    
     def clear_canvas(self):
         self.canvas.clear()
-        print('press')
+
+
+
+class LayItOn(BoxLayout):
+        pass
 
 
 
